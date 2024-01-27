8b268dd4f43b6dfb3df536462f712d884b3572b5
seohee <53520867+seohee99@users.noreply.github.com>
2024-01-27 22:39:29 +0900

    14503_로봇청소기.py 업데이트

--- Baekjoon/14503_로봇청소기.py
+++ Baekjoon/14503_로봇청소기.py
@@ -1,6 +1,43 @@
-N, M = map(int, input().split())
-r, c, d = map(int, input().split())
-arr = [ [ (0,0) for _ in range(N) ] for _ in range(M) ]
+import sys
+input = sys.stdin.readline
+from collections import deque
 
+n,m = map(int,input().split())
+graph = []
+visited = [[0] * m for _ in range(n)]
+r,c,d = map(int,input().split())
 
+# d => 0,3,2,1 순서
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
 
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
+
+# 방문 처리
+visited[r][c] = 1
+cnt = 1
+
+while 1:
+    flag = 0
+    # 4방향 확인
+    for _ in range(4):
+        nx = r + dx[(d+3)%4]
+        ny = c + dy[(d+3)%4]
+
+        d = (d+3)%4
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+            if visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                cnt += 1
+                r = nx
+                c = ny
+                #청소 한 방향이라도 했으면 다음으로 넘어감
+                flag = 1
+                break
+    if flag == 0: # 4방향 모두 청소가 되어 있을 때,
+        if graph[r-dx[d]][c-dy[d]] == 1: # 벽
+            print(cnt)
+            break
+        else:
+            r,c = r-dx[d],c-dy[d]
