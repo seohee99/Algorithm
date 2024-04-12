650027120d2cf35c6cc32cef67c9a4e162fb0b4b
seohee99 <imshee99@gmail.com>
2024-04-12 16:41:16 +0900

    [Feat] N과M 복습

--- Baekjoon/15649_N과M.py
+++ Baekjoon/15649_N과M.py
@@ -1,72 +1,24 @@
-# N과 M (1)
 import sys
-input = sys.stdin.readline
-N, M = map(int, input().split())
+input = sys.stdin.readline()
+
+N, M = map(int, input.split())
 
 visited = [False] * (N+1)
 answer = [] # 정답을 담을 stack
 
-# 재귀 함수로 숫자 선택 반복
-def dfs(n):
-    # 종료조건
-    if (len(answer) == M):
-        for _ in answer : print(_, end=" ")
+def dfs():
+    if len(answer) == M:
+        for i in answer:
+            print(i, end=' ')
         print()
         return
-    
+
     for i in range(1, N+1):
-        if visited[i]: continue
-        
+        if visited[i] == True:
+            continue
         visited[i] = True
         answer.append(i)
-        dfs(n+1) # 다음 꺼 뽑으러 다음 단계로
+        dfs()
         answer.pop()
         visited[i] = False
-
-dfs(0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# n, m = map(int, input().split())
-
-# s = [] # m개의 수열 저장하는 리스트
-# visited = [False] * (n+1)
-# def dfs():
-#     if len(s) == m: # 리스트에 m개가 append 되면 재귀 종료
-#         print(' '.join(map(str, s)))
-#         return
-
-#     for i in range(1, n+1):
-#         if visited[i]:
-#             continue
-#         visited[i] = True
-#         s.append(i)
-#         dfs()
-#         s.pop()
-#         visited[i] = False
-# dfs()
+dfs()
--- Baekjoon/15650_N과M2.py
+++ Baekjoon/15650_N과M2.py
@@ -1,72 +1,27 @@
-# N과 M (1)
+# N과 M (2)
+# 자연수 N, M이 주어졌을 때, 1부터 N까지 자연수 중에서 중복없이 M개를 고른 수열 출력(오름차순)
 import sys
-input = sys.stdin.readline
-N, M = map(int, input().split())
+input = sys.stdin.readline()
+
+N, M = map(int, input.split())
 
 visited = [False] * (N+1)
 answer = [] # 정답을 담을 stack
 
-# 재귀 함수로 숫자 선택 반복
 def dfs(n):
-    # 종료조건
-    if (len(answer) == M): # M개만큼 뽑았으면 정답 출력
-        for _ in answer : print(_, end=" ")
+    if len(answer) == M:
+        for i in answer:
+            print(i, end=' ')
         print()
         return
-    
-    for n in range(n, N+1): # for문의 범위를 n부터 해서 중복 방지 **
-        if visited[n]: continue # 이미 고른 숫자라면 continue
-        
-        visited[n] = True
-        answer.append(n)
-        dfs(n+1) # 다음 꺼 뽑으러 다음 단계로
-        answer.pop() # answer에 넣은 숫자는 pop해줘야 다음 숫자를 담을 수 있음!
-        visited[n] = False
 
+    for i in range(n, N+1): # 이전 자릿수보다 크게
+        if visited[i] == True:
+            continue
+        visited[i] = True
+        answer.append(i)
+        dfs(i+1) # i+1
+        answer.pop()
+        visited[i] = False
 dfs(1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# s = [] # m개의 수열 저장하는 리스트
-# visited = [False] * (n+1)
-# def dfs():
-#     if len(s) == m: # 리스트에 m개가 append 되면 재귀 종료
-#         print(' '.join(map(str, s)))
-#         return
-
-#     for i in range(1, n+1):
-#         if visited[i]:
-#             continue
-#         if len(s) > 0:
-#             if s[-1] > i:
-#                 continue
-#         visited[i] = True
-#         s.append(i)
-#         dfs()
-#         s.pop()
-#         visited[i] = False
-
-
-# dfs()
