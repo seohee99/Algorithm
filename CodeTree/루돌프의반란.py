af8b967b4f5c14204f0305e9354a279da4ed50f0
seohee99 <imshee99@gmail.com>
2024-04-13 19:10:57 +0900

    [Feat] 2023 하반기 오전 1번

--- CodeTree/루돌프의반란.py
+++ CodeTree/루돌프의반란.py
@@ -1,54 +1,39 @@
 # 2023 하반기 1번
 
-from collections import deque
-
-# 게임판 크기(N), 게임 턴 수(M), 산타 수(P), 루돌프 힘(C), 산타 힘(D)
-N, M, P, C, D = map(int, input().split())
-
-# 게임판(가장자리 벽 세워줌)
-board = [ [-1] * (N+2) for _ in range(N+2) ]
-
-# 루돌프 초기 위치
-Rr, Rc = map(int, input().split())
-rudolf_dict = {0: [Rr, Rc]}
-board[Rr][Rc] = 0
-
-santa_dict = {}
-# 산타 초기 위치 P만큼
-for  _ in range(P):
-    idx, Sr, Sc = map(int, input().split())
-    santa_dict[idx] = [Sr, Sc, 0] # 위치 r, 위치 c, 점수
-    board[Sr][Sc] = idx
-
 # 거리 체크
 def calculate_distance(r1, c1, r2, c2):
     return (r1 - r2) ** 2 + (c1 - c2) ** 2
 
 # 루돌프가 움직일 좌표 구하기
-def get_rudolf_move(Rr, Rc, santa_dict):
-
+def get_rudolf_move(Rr, Rc):
     ## 0. 방향 인덱스
     dRr = [-1, -1, -1, 0, 0, 1, 1, 1 ]
     dRc = [-1, 0, 1, -1, 1, -1, 0, 1]
 
     ## 1. 가장 가까운 산타 체크
-    dist = [5001] * (P+1) # 가장 큰 값으로 초기화
+    dist = {}
     for santa in santa_dict:
-        # 탈락한 산타(점수가 -1)인 산타 무시
-        if santa_dict[santa][2] != -1:
-            # 각각 산타와 루돌프의 거리 계산 후 dist 배열에 넣기
+        # 각각 산타와 루돌프의 거리 계산 후 dist 배열에 넣기
+        if santa_dict[santa][-1] == False:
             dist[santa] = calculate_distance(Rr, Rc, santa_dict[santa][0], santa_dict[santa][1])
-
+    # 루돌프 이동할 곳 없으면
+    if dist == {}:
+        return None
     # 최솟값 찾기
-    min_dist = min(dist)
-    # print(min_dist)
+    min_dist = min(dist.values())
     # 가장 가까운 산타 여러명이면
     near_santa = 0
-    if dist.count(min_dist) > 1:
+    near_santa_count = 0
+    for i in dist.values():
+        if i == min_dist:
+            near_santa_count += 1
+    if near_santa_count > 1:
         santa_list = []
-        for i in range(len(dist)):
-            if dist[i] == min_dist:
-                santa_list.append(i)
+        # print(len(dist))
+        for key, value in dist.items():
+            if value == min_dist:
+                santa_list.append(key)
+        # print(santa_list)
         # r(다음 c)이 큰 산타 찾기 : board를 (N,N)부터 거꾸로 읽음
         for i in range(N,0,-1):
             for j in range(N,0,-1):
@@ -57,121 +42,236 @@ def get_rudolf_move(Rr, Rc, santa_dict):
                     break
             if near_santa>0:
                 break
-        # print(near_santa)
-
-    ## 2. 루돌프 이동
-    # 산타와 가장 가까워지는 방향으로 이동
+    else:
+        for key, value in dist.items():
+            if value == min_dist:
+                near_santa = key
+    # 산타와 가장 가까워지는 방향 찾기
     rudolf_move_list = {}
     for i in range(8):
         nRr = Rr + dRr[i]
         nRc = Rc + dRc[i]
-        rudolf_move_list[ calculate_distance(nRr, nRc, santa_dict[near_santa][0],santa_dict[near_santa][1])] = [nRr, nRc]
+        rudolf_move_list[calculate_distance(nRr, nRc, santa_dict[near_santa][0],santa_dict[near_santa][1])] = [nRr, nRc]
     # key(거리)를 기준으로 정렬
-    min_rudolf_move_dist = sorted(rudolf_move_list.items())[0][0]
-    new_rudolf =  sorted(rudolf_move_list.items())[0][1]
+    new_rudolf = sorted(rudolf_move_list.items())[0][1]
     return new_rudolf
 
+# 산타 움직일 좌표 구하기
 def get_santa_move(Sr, Sc, Rr, Rc):
-    dSr = [-1, 1, 0, 0]
-    dSc = [0, 0, -1, 0]
+    # 좌 하 우 상
+    dSr = [0, 1, 0, -1]
+    dSc = [-1, 0, 1, 0]
     santa_move_list = {}
+    # 현재 위치부터 루돌프까지 거리도 넣기
+    santa_move_list[calculate_distance(Rr,Rc, Sr, Sc)] = [ Sr, Sc ]
     for i in range(4):
         nSr = Sr + dSr[i]
         nSc = Sc + dSc[i]
-        santa_move_list[calculate_distance(Rr,Rc, nSr, nSc)] = [ nSr, nSc ]
-    new_santa = sorted(santa_move_list.items())[0][1]
-    return new_santa
+        # print(nSr, nSc, calculate_distance(Rr,Rc, nSr, nSc))
+        if canMove(nSr, nSc) or (nSr == Sr and nSc == Sc):
+            santa_move_list[calculate_distance(Rr,Rc, nSr, nSc)] = [ nSr, nSc ]
+    santa_move_list = sorted(santa_move_list.items())
+    # print(santa_move_list)
+    for key, value in santa_move_list:
 
-def rufolf_conflict(Rr, Rc, santa_dict):
-    return
-
-def santa_conflict(Sr, Sc, santa_dict):
-    return
-
-# 이 함수를 분리해야겠군... 루돌프 / 산타로
-def conflict(r, c, idx, dr, dc):
-
-    # -1이 아니면 무언가 있다는 것
-    old = board[r][c]
-    print(old)
-    if old != -1:
-        # 루돌프가 움직여서 충돌이 난 경우
-        if idx == 0:
-            print("루돌프가 박았어요")
-            # 루돌프 이동
-            board[r][c] = idx
-            # 산타 점수 C만큼 주기
-            santa_dict[old][2] += C
-            # 산타 C만큼 밀려남
-            nr, nc = r + dr * C, c + dc * C
-            if nr > 0 and nr <= N and nc > 0 and nc < N:
-                print("이도잉요~")
-                board[nr][nc] = old
-                # 점수는 그대로, dict의 좌표 이동
-                santa_dict[old] = [nr, nc, santa_dict[old][2]]
-                # 또 산타 있으면 연쇄적 밀림 -> 상호작용...
-                if board[nr][nc] > 0:
-                    conflict(nr, nc, old, nr - r, nc - c)
-            # 탈락
-            else:
-                del(santa_dict[old])
-                print(old, "R.I.P")
-        # 산타가 움직여서 충돌이 난 경우
+        if canMove(value[0], value[1]) or (value[0] == Sr and value[1] == Sc):
+            new_santa = value
+            # print(new_santa,"여기로 움직일 예정이에요")
+            return new_santa
+    return None
+# 산타 움직일 수 있는 지 체크
+def canMove(r, c):
+    # 벽 아닌 경우
+    if (r > 0 and r <= N and c > 0 and c <= N):
+        # 움직이려는 곳에 산타 있으면 => X
+        if board[r][c] > 0:
+            return False
         else:
-            print(idx, "산타가 박았어요")
-            # 이동한 산타
-            board[r][c] = idx
-            # 산타 점수 D만큼 주기
-            santa_dict[old][2] += D
-            # 산타 D만큼 밀려남
-            nr, nc = r + dr * D, c + dc * D
-            if nr > 0 and nr <= N and nc > 0 and nc < N:
-                print("이동이요~")
-                board[nr][nc] = old
-                # 점수는 그대로, dict의 좌표 이동
-                santa_dict[old] = [nr, nc, santa_dict[old][2]]
-                # 또 산타 있으면 연쇄적 밀림
-                if board[nr][nc] > 0:
-                    conflict(nr, nc, old, nr - r, nc - c)
-            # 탈락
-            else:
-                del(santa_dict[old])
-                print(old, "R.I.P")
+            return True
+    # 벽인 경우
     else:
-        # 종료 조건 : 충돌 안날때 까지
-        print("충돌 안났어요")
+        return False
+
+def rudolf_move(Rr, Rc):
+    # 루돌프가 이동할 좌표 찾음
+    new_rudolf_place = get_rudolf_move(Rr, Rc)
+    if new_rudolf_place == None:
         return
+    nRr, nRc = new_rudolf_place[0], new_rudolf_place[1]
+    # print("루돌프 좌표",Rr, Rc, nRr, nRc)
+    # 충돌 처리하고
+    direction = [nRr - Rr, nRc - Rc]  # 이동할 방향 벡터
+    conflict(nRr, nRc, direction, 0, C)   # 충돌 먼저 처리
+    # 루돌프 이동
+    board[Rr][Rc], board[nRr][nRc] = -1, 0 # 보드에 표시하고
+    rudolf_dict[0], rudolf_dict[1] = nRr, nRc # 루돌프 정보 업데이트
+    return
+
+def santa_move(Sr, Sc, santa):
+    # print(santa, "움직입니다", santa_dict)
+    # 움직일 산타 위치 찾기
+    new_santa_place = get_santa_move(Sr, Sc, rudolf_dict[0], rudolf_dict[1])
+    if new_santa_place == None:
+        # print("못움직이므로 종료")
+        return
+    nSr, nSc = new_santa_place[0], new_santa_place[1]
+    # print(nSr, nSc, board[3][2])
+    # 움직일 수 있어도 루돌프랑 가까워질 수 있는 방법 없으면 움직이지 않음
+    if (nSr == Sr) and (nSc == Sc):
+        # print("가까워질 방법 없어 종료")
+        return
+    else:
+        direction = [nSr - Sr, nSc - Sc]
+        conflict(nSr, nSc, direction, santa, D)
+        # 산타 옮기기
+        # 만약 이동할 곳에 루돌프 있다면, 이동할 산타가 반대로 밀려남 => conflict 안에서 처리하고 이 함수에서는 처리 안함
+        if board[nSr][nSc] == 0:
+            return
+        else:
+            board[Sr][Sc], board[nSr][nSc] = -1, santa
+            santa_dict[santa][0], santa_dict[santa][1] = nSr, nSc
+            return
+# 산타와 루돌프가 같이 있다면 충돌!
+def conflict(r, c, d, idx, power):
+
+    # 충돌 체크
+    if board[r][c] == -1:
+        # print("충돌 안남")
+        return
+
+    # 루돌프가 달려왔다면
+    if idx == 0:
+        # 해당 산타 C 만큼 점수 얻어
+        santa_dict[board[r][c]][2] += C
+        # print(board[r][c], santa_dict[board[r][c]], "충돌일어남")
+        # 기절
+        santa_stun_list[2].append(board[r][c])
+        # print(santa_stun_list)
+        # 산타가 이동해온 방향으로 C칸 밀려나
+        nSr, nSc = r + d[0] * C, c + d[1] * C
+        if (nSr > 0 and nSr <= N and nSc > 0 and nSc <= N):
+            # 상호작용 체크하고
+            interaction(nSr, nSc, board[nSr][nSc], [d[0], d[1]])
+            # 산타 이동
+            santa_dict[board[r][c]][0], santa_dict[board[r][c]][1] = nSr, nSc
+            board[nSr][nSc] = board[r][c]
+            # 루돌프 이동
+            board[r][c], board[rudolf_dict[0]][rudolf_dict[1]] = 0, -1
+            rudolf_dict[0], rudolf_dict[1]= r, c
+            return
+        else:
+            # 산타 탈락
+            santa_dict[board[r][c]][-1], santa_dict[board[r][c]][0], santa_dict[board[r][c]][1] = True, 0, 0
+            # print(board[r][c], "산타가 게임판 밖으로 밀려나서 탈락했습니다.")
+            board[r][c] = -1
+    # 산타가 달려왔다면
+    else:
+        # print(idx, "산타 -> 루돌프 충돌이요~")
+        # 해당 산타 D 만큼 점수 얻어
+        santa_dict[idx][2] += D
+        # print(santa_dict[idx]) # OK
+        # 기절
+        santa_stun_list[2].append(idx)
+        # print(santa_stun_list) # OK
+        # 산타 이동해 온 반대 방향으로 D 만큼 밀려나
+        nSr, nSc = r - d[0] * D, c - d[1] * D
+        # print(D, "만큼 여기로 밀려납니다ㅠㅠ", nSr, nSc) # OK
+
+        if (nSr > 0 and nSr <= N and nSc > 0 and nSc <= N):
+            # 상호작용 체크하고
+            # print("이제 상호작용 체크합니다")
+            board[santa_dict[idx][0]][santa_dict[idx][1]] = -1
+            interaction(nSr, nSc, board[nSr][nSc],[-d[0], -d[1]])
+            santa_dict[idx][0], santa_dict[idx][1] = nSr, nSc
+            board[nSr][nSc] = idx
+            # 산타 이동
+            # print("WHHHH",board)
+            return
+        else:
+            # 산타 탈락
+            santa_dict[idx][-1] = True
+            # print(idx, "산타가 게임판 밖으로 밀려나서 탈락했습니다.")
+            board[santa_dict[idx][0]][santa_dict[idx][1]] = -1
+            santa_dict[idx][0], santa_dict[idx][1] = 0, 0
 # 상호작용
-def interaction():
-    return
-# 기절
-def faint():
-    # 한 턴 돌때마다 이동
-    faint_list = {2 : [], 1 : []}
-    return
+def interaction(r, c, santa, d):
+    # print(r, c,"에있는", santa,"상호작용 시작, 방향",d)
+    # 연쇄 밀림
+    # 충돌 체크(연쇄할 거 없으면 return : 종료 조건)
+    if board[r][c] == -1:
+        return
+    # 연쇄(재귀)
+    direction = d
+    interaction(r + d[0], c + d[1], board[r + d[0]][c + d[1]], direction)
+    santa_dict[santa][0], santa_dict[santa][1] = r + d[0], c + d[1]
+    board[r+d[0]][c+d[1]] = santa
+    # print(santa, santa_dict[santa],board[r+d[0]][c+d[1]],r+d[0],c+d[1],"연쇄 이동완.")
+    # print(board)
 
 def main():
-    print(santa_dict)
-    #### 1. 루돌프  옮기기
-    Rr, Rc = rudolf_dict[0][0], rudolf_dict[0][1]
-    new_rudolf_place = get_rudolf_move(Rr, Rc, santa_dict)
-    # 이전 루돌프 좌표 벽으로 만들고
-    board[Rr][Rc] = -1
-    # 루돌프 옮기기
-    # ㅅㅂ 충돌 체크
-    conflict(Rr, Rc, 0, new_rudolf_place[0] - Rr, new_rudolf_place[1] - Rc)
-    rudolf_dict[0][0], rudolf_dict[0][1] = new_rudolf_place[0], new_rudolf_place[1]
-    Rr, Rc = rudolf_dict[0][0], rudolf_dict[0][1]
-    # board[Rr][Rc] = 0
+    global santa_dict, board, rudolf_dict, santa_stun_list, N, M, P, C, D, board
+    # 게임판 크기(N), 게임 턴 수(M), 산타 수(P), 루돌프 힘(C), 산타 힘(D)
+    N, M, P, C, D = map(int, input().split())
 
-    #### 2. 산타 차례로 옮기기
-    for santa in santa_dict:
-        print(santa, "옮길 위치 구합니다!")
-        new_santa_place = get_santa_move(santa_dict[santa][0],santa_dict[santa][1],Rr,Rc)
-        print(new_santa_place)
+    # 게임판(가장자리 벽 세워줌)
+    board = [[-1] * (N + 2) for _ in range(N + 2)]
 
-        conflict(santa_dict[santa][0],santa_dict[santa][1], santa, new_santa_place[0] - santa_dict[santa][0], new_santa_place[1] - santa_dict[santa][1])
-        board[santa_dict[santa][0]][santa_dict[santa][1]] = -1
-    print(santa_dict)
+    # 루돌프 초기 위치
+    Rr, Rc = map(int, input().split())
+    rudolf_dict = [Rr, Rc]
+    board[Rr][Rc] = 0
 
+    # 산타 초기 위치 P만큼
+    santa_dict = {}
+    for _ in range(P):
+        idx, Sr, Sc = map(int, input().split())
+        santa_dict[idx] = [Sr, Sc, 0, False]  # 위치 r, 위치 c, 점수, isSurvived
+        board[Sr][Sc] = idx
+
+    santa_stun_list = {2: [], 1: []}
+    santa_dict = dict(sorted(santa_dict.items()))
+
+    for c in range(1,M+1):
+        # print(c, "#################################################333")
+        ### 0. 한 턴이 돌때마다 기절 산타 상태 바꿔주기
+        santa_stun_list[1] = santa_stun_list[2]
+        santa_stun_list[2] = []
+        #### 1. 루돌프  옮기기
+        rudolf_move(rudolf_dict[0], rudolf_dict[1])
+        # print("루돌프 움직였어요 ::", rudolf_dict)
+        #### 2. 산타 차례로 옮기기
+        for santa in santa_dict:
+            # print("======================", santa, "=============")
+            if santa_dict[santa][-1] == True:
+                # print(santa, "탈락산타")
+                continue
+            if santa not in (santa_stun_list[1]) and santa not in (santa_stun_list[2]):
+                # print(santa, "움직임")
+                Sr, Sc = santa_dict[santa][0], santa_dict[santa][1]
+                santa_move(Sr, Sc, santa)
+                # print(santa_dict)
+                # print(Sr, Sc, "에서", santa_dict[santa][0], santa_dict[santa][1]," 여기로")
+
+        ### 4. 모두 탈락했다면 종료 / 매턴 이후 탈락하지 않은 산타 1점 부여
+        if len(santa_dict) == 0:
+            # print("모든 산타가 탈락하여 종료합니다.")
+            break
+        else:
+            for santa in santa_dict:
+                if santa_dict[santa][-1] == False:
+                    santa_dict[santa][2] += 1
+
+        # print("****한 턴 결과****")
+        # print(santa_dict)
+        # print(santa_stun_list)
+        # for i in range(1,len(board)-1):
+        #     for j in range(1,len(board[i])-1):
+        #         print(board[i][j], end=' ')
+        #     print()
+        # print("****************")
+    answer = 0
+    if len(santa_dict) > 0:
+        for santa in santa_dict:
+            print(santa_dict[santa][2], end=' ')
+    # print("SANTA", santa_dict)
 main()
